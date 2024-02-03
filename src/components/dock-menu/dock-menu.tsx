27c610ba77d234f9ed7c9cbe473a3065f194ae16
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-02-03 22:53:46 +0100

    feat: use tooltips in navigation

--- src/components/dock-menu/dock-menu.tsx
+++ src/components/dock-menu/dock-menu.tsx
@@ -14,7 +14,13 @@ import styles from "./dock-menu.module.css";
 import Link from "next/link";
 import { CodeEditor } from "../code-editor/code-editor";
 import { captureElement } from "@/lib/utils";
-import { useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import {
+  Button,
+  Tooltip,
+  UnstyledButton,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
 
 interface DockMenuProps {
   position?: "top" | "bottom";
@@ -38,47 +44,71 @@ export default function DockMenu({}: DockMenuProps) {
       <div className={styles.navContainer}>
         <CodeEditor
           triggerChild={
-            <div className={styles.navElement}>
-              <IconCode className={styles.navIcon} />
-            </div>
+            <Tooltip position="top" offset={12} label="Code Editor">
+              <UnstyledButton className={styles.navElement}>
+                <IconCode className={styles.navIcon} />
+              </UnstyledButton>
+            </Tooltip>
           }
         />
 
-        <button
-          className={styles.navElement}
-          onClick={() => captureElement("diff-view")}
-        >
-          <IconCameraCode className={styles.navIcon} />
-        </button>
-        <div className={styles.navElement}>
-          <IconSettings className={styles.navIcon} />
-        </div>
+        <Tooltip position="top" offset={12} label="Export Image">
+          <UnstyledButton
+            className={styles.navElement}
+            onClick={() => captureElement("diff-view")}
+          >
+            <IconCameraCode className={styles.navIcon} />
+          </UnstyledButton>
+        </Tooltip>
+
+        <Tooltip position="top" offset={12} label="Settings">
+          <UnstyledButton className={styles.navElement}>
+            <IconSettings className={styles.navIcon} />
+          </UnstyledButton>
+        </Tooltip>
+
         <div className={styles.navSeparator}></div>
-        <Link
-          className={styles.navElement}
-          href={"https://github.com/anand-san/beautiful-git-diffs"}
-          target="_blank"
-        >
-          <IconBrandGithub className={styles.navIcon} />
-        </Link>
-        <Link
-          className={styles.navElement}
-          href={"https://twitter.com/anandsan_"}
-          target="_blank"
-        >
-          <IconBrandX className={styles.navIcon} />
-        </Link>
+
+        <Tooltip position="top" offset={12} label="Github">
+          <Link
+            className={styles.navElement}
+            href={"https://github.com/anand-san/beautiful-git-diffs"}
+            target="_blank"
+          >
+            <IconBrandGithub className={styles.navIcon} />
+          </Link>
+        </Tooltip>
+
+        <Tooltip position="top" offset={12} label="X">
+          <Link
+            className={styles.navElement}
+            href={"https://twitter.com/anandsan_"}
+            target="_blank"
+          >
+            <IconBrandX className={styles.navIcon} />
+          </Link>
+        </Tooltip>
+
         <div className={styles.navSeparator}></div>
-        <Link className={styles.navElement} href={"/login"}>
-          <IconLogin2 className={styles.navIcon} />
-        </Link>
-        <button className={styles.navElement} onClick={toggleColorScheme}>
-          {computedColorScheme && computedColorScheme === "light" ? (
-            <IconMoon className={styles.navIcon} />
-          ) : (
-            <IconSun className={styles.navIcon} />
-          )}
-        </button>
+
+        <Tooltip position="top" offset={12} label="Login">
+          <Link className={styles.navElement} href={"/login"}>
+            <IconLogin2 className={styles.navIcon} />
+          </Link>
+        </Tooltip>
+
+        <Tooltip position="top" offset={12} label="Toggle Theme">
+          <UnstyledButton
+            className={styles.navElement}
+            onClick={toggleColorScheme}
+          >
+            {computedColorScheme && computedColorScheme === "light" ? (
+              <IconMoon className={styles.navIcon} />
+            ) : (
+              <IconSun className={styles.navIcon} />
+            )}
+          </UnstyledButton>
+        </Tooltip>
       </div>
     </section>
   );
