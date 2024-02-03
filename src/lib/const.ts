a6916dd3cdbe448a54abdc4c19acc2be9c320609
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-02-03 09:33:15 +0100

    fix: move sample source and target code into utils

---
+++ src/lib/const.ts
@@ -0,0 +1,9 @@
+export const sampleSourceCode = `export const OldCode = () => {
+    // Not so amazing code
+    // Change me
+  };`;
+
+export const sampleTargetCode = `export const NewCode = () => {
+    // better code maybe?
+    // Change me
+  };`;
