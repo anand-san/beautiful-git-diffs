6457529d20ec48fb29c23502df9f66c60341b6dc
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-02-03 16:12:05 +0100

    feat: remove hardcoded dark background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-800">
+    <html lang="en">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
