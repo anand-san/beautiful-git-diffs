e10990083f0d5c26e122846be0877303378a1b2a
Anand Sandilya <anand.sandilya@quantilope.com>
2024-02-03 16:09:33 +0100

    feat: move diff util functions inside utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { toPng } from "html-to-image";
+import Prism from "prismjs";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const captureElement = async (elementId: string) => {
+  const node = document.getElementById(elementId) as HTMLElement;
+  const dataUrl = await toPng(node);
+  const link = document.createElement("a");
+  link.download = "diff.png";
+  link.href = dataUrl;
+  link.click();
+};
+
+export const customHighlightSyntax = (str: string) => {
+  if (!str) str = "";
+  return Prism.highlight(str, Prism.languages.javascript, "javascript");
+};
