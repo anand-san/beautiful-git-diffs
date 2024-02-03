f830ec7fe69599abe62c81a82b0db70d34c934ee
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-02-03 16:08:40 +0100

    feat: add theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-800">
       <body className={inter.className}>
-        {children} <Analytics />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
